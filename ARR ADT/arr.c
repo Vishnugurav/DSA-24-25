f8f35a3fd04ecd1ce9298c5ef7ef9cf6651ed8d4
Vishnugurav <vishnugurav4241@gmail.com>
2024-12-10 22:46:27 +0530

    Resolved merge conflicts

--- ARR ADT/arr.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-void init(Array *arr ){
-    arr->A = (int*)malloc(sizeof(int) * size);
-    arr->size= 20;
-    arr-> length = 0;
-}
--- CIRCULAR LL/hello.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("Vishnu Gurav");
-    return 0;
-}
